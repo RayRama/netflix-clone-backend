04bae2ba6c601cbf96b0cf8f013e990d3299c5ed
RayRama <rayramadita12@gmail.com>
2023-06-01 15:32:04 +0700

    Initial Release

---
+++ api/controllers/UserController/GetUser.js
@@ -0,0 +1,17 @@
+class GetUser {
+  constructor(User) {
+    this.User = User;
+  }
+
+  async execute(req, res) {
+    try {
+      const user = await this.User.findById(req.params.id);
+      const { password, ...others } = user._doc;
+      res.status(200).json(others);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+}
+
+module.exports = GetUser;
