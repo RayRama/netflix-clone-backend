f43a99867092b2140b53231bb493a72eeeedb708
RayRama <rayramadita12@gmail.com>
2023-12-26 17:48:13 +0700

    Fix typo

--- api/routes/lists.js
+++ api/routes/lists.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const verify = require("../verifyToken");
-const ListController = require("../controllers/listController");
+const ListController = require("../controllers/ListController");
 
 const listController = new ListController();
 
